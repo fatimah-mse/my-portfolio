c83f1c521963b3b14ffdbf4c1bc142f6337cd867
fatimah-mse <fatimahmse83@gmail.com>
2024-11-05 00:28:01 +0300

    edit

--- src/components/HeroComoponent/HeroComoponent.jsx
+++ src/components/HeroComoponent/HeroComoponent.jsx
@@ -4,7 +4,7 @@ import './HeroComoponent.css'
 export default function HeroComoponent({title , name , desc , img , myclass}) {
     return (
         <section className={`FM-padding FM-hero ${myclass ? 'FM-reverse FM-hero-bg' : ''}`}>
-            <div className='FM-description'>
+            <div className={myclass ? '' : 'FM-description'}>
                 <h5 className='FM-h5'>{title}</h5>
                 <h1 className='FM-h1'>{name}</h1>
                 <p className={`FM-p ${myclass ? 'FM-p-reverse' : ''}`}>{desc}</p>
